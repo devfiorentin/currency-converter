ea8e78c012db8bf577f79bb1fae1008c5b8ba9e4
devmolozi <jeancordeiro.f@gmail.com>
2025-03-15 02:29:38 -0300

    app css

--- src/components/SelectInput.tsx
+++ src/components/SelectInput.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import "../App.css";
+import "App.css";
 
 function SelectInput() {
   const [fromCurrency, setFromCurrency] = useState("BRL");
