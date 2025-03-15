e53c0989f0ac51b2ff055d7f3a5df2661d862449
devmolozi <jeancordeiro.f@gmail.com>
2025-03-15 14:24:37 -0300

    redeploy

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,10 @@ function Header() {
   return (
     <div className="p-10 flex items-center justify-center">
       <div className="text-left py-10 px-5 justify-left items-left flex flex-col w-full max-w-2xl">
-        <h1 className="text-[#9FE870] font-bold text-4xl p-2">CONVERTE AI</h1>
-        <p className="text-[#9FE870] font-bold text-3xl p-2">
+        <h1 className="text-[#9FE870] font-bold text-3xl md:text-5xl p-2">
+          CONVERTE AI
+        </h1>
+          <p className="text-[#9FE870] font-bold text-2xl md:text-4xl p-2">
           Converta com facilidade e rapidez!
         </p>
       </div>
